1428d3db6ea1e1307835ddb98edb3e707ab13e42
Javier Plaza Sisqués <javierplazasisque@gmail.com>
2019-05-09 21:15:54 +0200

    En proceso de los dos modelos

--- src/modelo/ModeloConsultas.java
+++ src/modelo/ModeloConsultas.java
@@ -1,5 +1,56 @@
 package modelo;
 
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+import controlador.Controlador;
+import vista.CrearUsuario;
+import vista.Gestion;
+import vista.GestionAcad;
+import vista.GestionActividad;
+import vista.GestionActores;
+import vista.GestionAlumnos;
+import vista.GestionAsignatura;
+import vista.GestionProfesores;
+import vista.GestionRegistros;
+import vista.GestionSalas;
+import vista.GestionUsuarios;
+import vista.Home;
+import vista.InformacionExtra;
+import vista.Informes;
+import vista.Login;
+import vista.Perfil;
+import vista.VerGrupos;
+
 public class ModeloConsultas {
+
+	// Atributos para relacionar
+	private Login login;
+	private Home home;
+	private Gestion gestion;
+	private Informes informes;
+//	private Ocupaciones ocupaciones;
+	private InformacionExtra infoExtra;
+	private CrearUsuario crearUsuario;
+	private GestionUsuarios gestionUsuarios;
+	private GestionAcad gestionAcad;
+	private GestionActividad gestionActividad;
+	private GestionActores gestionActores;
+	private GestionAlumnos gestionAlumnos;
+	private GestionRegistros gestionRegistros;
+	private GestionAsignatura gestionAsignatura;
+	private GestionProfesores gestionProfesores;
+	private GestionSalas gestionSalas;
+	private VerGrupos verGrupos;
+	private Perfil perfil;
+	private Controlador controlador;
+
+	// Atributos Fichero
+	private Properties propiedades;
+	private InputStream entrada;
+	private OutputStream salida;
+	private File fichero;
 
 }
