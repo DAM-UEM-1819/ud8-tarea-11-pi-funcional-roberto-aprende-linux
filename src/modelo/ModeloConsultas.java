0af5e617b63f133633dd3916f348163b13db3245
V1tter <di.viteri.dv@gmail.com>
2019-05-11 23:45:04 +0200

    terminados las propiedades

--- src/modelo/ModeloConsultas.java
+++ src/modelo/ModeloConsultas.java
@@ -40,7 +40,7 @@ public class ModeloConsultas {
 	private Home home;
 	private Gestion gestion;
 	private Informes informes;
-//	private Ocupaciones ocupaciones;
+	// private Ocupaciones ocupaciones;
 	private InformacionExtra infoExtra;
 	private CrearUsuario crearUsuario;
 	private GestionUsuarios gestionUsuarios;
@@ -78,17 +78,16 @@ public class ModeloConsultas {
 	private String selectPasswdUsuario;
 
 	// Sentencias Select SQL TABLAS
-	private String selectHome ;
-	private String selectTodosUsuarios ;
-	private String selectTodosRegistros  ;
-	private String selectTodosAlumnos ;
-	private String selectTodasActividades ;
-	private String selectTodasAsignaturas ;
-	private String selectTodosProfesores ;
+	private String selectHome;
+	private String selectTodosUsuarios;
+	private String selectTodosRegistros;
+	private String selectTodosAlumnos;
+	private String selectTodasActividades;
+	private String selectTodasAsignaturas;
+	private String selectTodosProfesores;
 	private String selectTodosActores;
 	private String selectTodasSalas;
 	private String selectTodosAcad;
-	
 
 	private String selectTodosCodigoGrupo;
 
@@ -98,14 +97,14 @@ public class ModeloConsultas {
 	// Sentencias Select SQL BUSCADOR
 	private String selectBuscadorHome;
 	private String selectBuscadorUsuarios;
-	private String selectBuscadorRegistros ;
+	private String selectBuscadorRegistros;
 	private String selectBuscadorActividades;
-	private String selectBuscadorAsignatura = "SELECT codigo , nombre , titulacion , curso FROM HOSPITAL.asignatura WHERE codigo=? or nombre=? or titulacion=? or curso=?";
-	private String selectBuscadorAlumnos = "SELECT * FROM HOSPITAL.alumno WHERE exp=? or nombre=? or activo=?";
-	private String selectBuscadorProfesores = "SELECT * from HOSPITAL.profesor WHERE numero=? or nombre=? or apellido1=? or apellido2=? or titulacion=? or dni=? or activo=? or relacion_laboral=? or tlf1=? or tlf2=? or mail1=? or mail2=?";
-	private String selectBuscadorActores = "SELECT cod_actor, nombre, edad, genero, idioma, complexion, activo FROM HOSPITAL.actor WHERE cod_actor=? or nombre=? or edad=? or genero=? or idioma=? or complexion=? or activo=?";
-	private String selectBuscadorSalas = "SELECT cod_sala , tipo_sala , numero , capacidad FROM HOSPITAL.sala WHERE cod_sala=? or tipo_sala=? or numero=? or capacidad=?";
-	private String selectBuscadorAcad = "SELECT * FROM HOSPITAL.acad WHERE acad=? or sem1=? or sem2=?";
+	private String selectBuscadorAsignatura;
+	private String selectBuscadorAlumnos;
+	private String selectBuscadorProfesores;
+	private String selectBuscadorActores;
+	private String selectBuscadorSalas;
+	private String selectBuscadorAcad;
 
 	public ModeloConsultas() {
 		propiedades = new Properties();
@@ -124,11 +123,20 @@ public class ModeloConsultas {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		//Asignamos select de login usuario
+
+		// Asignamos select de login usuario
 		selectPasswdUsuario = propiedades.getProperty("selectPasswdUsuario");
-		
-		//Asignamos select de tablas
+
+		// Asignamos select de tablas
+		selectTablas();
+		// Asignamos select de listado
+		selectTodosCodigoGrupo = propiedades.getProperty("selectTodosCodigoGrupo");
+		selectListadoAlumnosPorGrupo = propiedades.getProperty("selectListadoAlumnosPorGrupo");
+		// Asignamos las Select SQL BUSCADOR
+		selectBuscador();
+	}
+    
+	public void selectTablas() {
 		selectHome = propiedades.getProperty("selectHome");
 		selectTodosUsuarios = propiedades.getProperty("selectTodosUsuarios");
 		selectTodosRegistros = propiedades.getProperty("selectTodosRegistros");
@@ -138,19 +146,21 @@ public class ModeloConsultas {
 		selectTodosProfesores = propiedades.getProperty("selectTodosProfesores");
 		selectTodosActores = propiedades.getProperty("selectTodosProfesores");
 		selectTodasSalas = propiedades.getProperty("SelecTodasSalas");
-		selectTodosAcad =  propiedades.getProperty("selecTodosAcad");
-		//Asignamos select de listado
-		selectTodosCodigoGrupo = propiedades.getProperty("selectTodosCodigoGrupo");
-		selectListadoAlumnosPorGrupo = propiedades.getProperty("selectListadoAlumnosPorGrupo");
-		//Asignamos las Select SQL BUSCADOR
+		selectTodosAcad = propiedades.getProperty("selecTodosAcad");
+	}   	
+	
+	public void selectBuscador() {
 		selectBuscadorHome = propiedades.getProperty("selectBuscadorHome");
 		selectBuscadorUsuarios = propiedades.getProperty("selectBuscadorUsuarios");
 		selectBuscadorRegistros = propiedades.getProperty("selectBuscadorRegistros");
 		selectBuscadorActividades = propiedades.getProperty("selectBuscadorActividades");
-		
-		
-		
-	}
+		selectBuscadorAsignatura = propiedades.getProperty("selectBuscadorAsignatura");
+		selectBuscadorAlumnos = propiedades.getProperty("selectBuscadorAlumnos");
+		selectBuscadorProfesores = propiedades.getProperty("selectBuscadorProfesores");
+		selectBuscadorActores = propiedades.getProperty("selectBuscadorActores");
+		selectBuscadorSalas = propiedades.getProperty("selectBuscadorSalas");
+		selectBuscadorAcad = propiedades.getProperty("selectBuscadorAcad");
+	}   
 
 	// INICIO SETTERS
 
@@ -225,7 +235,7 @@ public class ModeloConsultas {
 	public void setControlador(Controlador controlador) {
 		this.controlador = controlador;
 	}
-	
+
 	public void setModelo(Modelo modelo) {
 		this.modelo = modelo;
 	}
@@ -241,7 +251,7 @@ public class ModeloConsultas {
 	}
 
 	// INICIO METODOS BASE DATOS
-
+	
 	public void loginConfirmacion(String usuario, String passwd) {
 		conexion = modelo.getConexion();
 		String sql = selectPasswdUsuario;
@@ -269,7 +279,7 @@ public class ModeloConsultas {
 			e.printStackTrace();
 		}
 	}
-
+	
 	/*
 	 * 
 	 * public void crearUsuario(String user, String passwd, String rol) { String sql
@@ -289,62 +299,52 @@ public class ModeloConsultas {
 
 	public void getTablaHome(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectHome;
-		getDatos(sql);
+		getDatos(selectHome);
 	}
 
 	public void getTablaUsuarios(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodosUsuarios;
-		getDatos(sql);
+		getDatos(selectTodosUsuarios);
 	}
 
 	public void getTablaAcad(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodosAcad;
-		getDatos(sql);
+		getDatos(selectTodosAcad);
 	}
 
 	public void getTablaActividad(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodasActividades;
-		getDatos(sql);
+		getDatos(selectTodasActividades);
 	}
 
 	public void getTablaActores(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodosActores;
-		getDatos(sql);
+		getDatos(selectTodosActores);
 	}
 
 	public void getTablaAlumnos(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodosAlumnos;
-		getDatos(sql);
+		getDatos(selectTodosAlumnos);
 	}
 
 	public void getTablaAsignatura(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodasAsignaturas;
-		getDatos(sql);
+		getDatos(selectTodasAsignaturas);
 	}
 
 	public void getTablaProfesores(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodosProfesores;
-		getDatos(sql);
+		getDatos(selectTodosProfesores);
 	}
 
 	public void getTablaRegistros(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodosRegistros;
-		getDatos(sql);
+		getDatos(selectTodosRegistros);
 	}
 
 	public void getTablaSalas(DefaultTableModel tableModel) {
 		this.tableModel = tableModel;
-		String sql = selectTodasSalas;
-		getDatos(sql);
+		getDatos(selectTodasSalas);
 	}
 
 	public void listadoGrupos() {
@@ -387,9 +387,6 @@ public class ModeloConsultas {
 				for (int i = 0; i < numColumnas; i++) {
 					tableModel.addColumn(metadatos.getColumnName(i + 1));
 				}
-
-				// metadatos.getColumnName(1);
-
 				while (rs.next()) {
 					for (int i = 0; i < numColumnas; i++) {
 						datosFilasTabla[i] = rs.getObject(i + 1);
@@ -402,7 +399,7 @@ public class ModeloConsultas {
 			}
 		}
 	}
-
+	
 	private void getDatos(String sql) {
 		tableModel.setColumnCount(0);
 		tableModel.setRowCount(0);
@@ -418,18 +415,14 @@ public class ModeloConsultas {
 				tableModel.addColumn(metadatos.getColumnName(i + 1));
 			}
 
-			// metadatos.getColumnName(1);
-
 			while (rs.next()) {
 				for (int i = 0; i < numColumnas; i++) {
 					datosFilasTabla[i] = rs.getObject(i + 1);
 				}
 				tableModel.addRow(datosFilasTabla);
 			}
-
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 }
